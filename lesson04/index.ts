894822f9cfe59227b6ab1d2a78734ba66777ae63
xinuo <vax_sayno@163.com>
2020-06-08 10:43:39 +0800

    xinuo

--- lesson04/index.ts
+++ lesson04/index.ts
@@ -2,3 +2,12 @@ var fun = ():void => {
   console.log(123);
 }
 fun();
+
+let fun1 = (...numArr: number[]): string => {
+  let sum = 0;
+  for(let i = 0; i < numArr.length; i++) {
+    sum += numArr[i];
+  }
+  return `${sum}px`;
+}
+console.log(fun1(1,2,333,4,1,2,3,2))
--- lesson04/js/index.js
+++ lesson04/js/index.js
@@ -3,3 +3,15 @@ var fun = function () {
     console.log(123);
 };
 fun();
+var fun1 = function () {
+    var numArr = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numArr[_i] = arguments[_i];
+    }
+    var sum = 0;
+    for (var i = 0; i < numArr.length; i++) {
+        sum += numArr[i];
+    }
+    return sum + "px";
+};
+console.log(fun1(1, 2, 333, 4, 1, 2, 3, 2));
